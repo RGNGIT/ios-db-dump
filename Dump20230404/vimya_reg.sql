d80abdaf1a940f3dbe8a79167510a35a9d787969
RGNGIT <vnifedow@bk.ru>
2023-04-04 13:32:24 +0500

    New dump

---
+++ Dump20230404/vimya_reg.sql
@@ -0,0 +1,54 @@
+-- MySQL dump 10.13  Distrib 8.0.32, for Win64 (x86_64)
+--
+-- Host: 127.0.0.1    Database: vimya
+-- ------------------------------------------------------
+-- Server version	8.0.32
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!50503 SET NAMES utf8 */;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+
+--
+-- Table structure for table `reg`
+--
+
+DROP TABLE IF EXISTS `reg`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `reg` (
+  `Reg_Date` datetime DEFAULT NULL,
+  `Phys_Key` int NOT NULL AUTO_INCREMENT,
+  `Login` text,
+  `Password` text,
+  PRIMARY KEY (`Phys_Key`),
+  CONSTRAINT `reg_fk1` FOREIGN KEY (`Phys_Key`) REFERENCES `phys` (`Key`)
+) ENGINE=InnoDB AUTO_INCREMENT=10 DEFAULT CHARSET=utf8mb3 PACK_KEYS=0;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `reg`
+--
+
+LOCK TABLES `reg` WRITE;
+/*!40000 ALTER TABLE `reg` DISABLE KEYS */;
+INSERT INTO `reg` VALUES ('2023-03-12 14:14:50',3,'zalupaKonya1','WryayicsKcsaAkfgqilTsX9xH2e1lDlXuUqJgXJS55qxcPacqHGjrTyBbNeA5aF7CFKGOxTKZiYyLo2Yqcjs5g=='),('2023-03-12 22:40:06',5,'zalupaKonya2','WryayicsKcsaAkfgqilTsX9xH2e1lDlXuUqJgXJS55qxcPacqHGjrTyBbNeA5aF7CFKGOxTKZiYyLo2Yqcjs5g==');
+/*!40000 ALTER TABLE `reg` ENABLE KEYS */;
+UNLOCK TABLES;
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+
+-- Dump completed on 2023-04-04 13:30:18
